a6cabbbb09a6e2b9655dcaf9be03b3f7db93cb98
CookieCrumbs19212 <developer19212@gmail.com>
2022-08-15 20:05:34 +0200

    Moved TimeFrame class from utils.py to timeframe.py
    Added get_times_str() and get_norm_times_str() methods to get string representations of the respective values.
    
    Refactored code to remove the need for the Timezone class.
    
    Added OUT_DATETIME_FORMAT.

---
+++ src/timeframe.py
@@ -0,0 +1,95 @@
+from typing import Tuple
+from datetime import datetime, timedelta
+
+# Input Datetime formats.
+IN_DATETIME_FORMAT = '%d-%m-%y %H%M'
+OUT_DATETIME_FORMAT = '%d-%m-%y %H:%M'
+
+
+class TimeFrame:
+    """
+    TimeFrame class that encapsulates the UTC offset, start time and end time of a timeframe.
+    """
+
+    def __init__(self, utc_offset: str, start_time: datetime | str, end_time: datetime | str) -> None:
+        """ Initialize a TimeFrame object with 3 mandatory parameters.
+
+        Args:
+            utc_offset (str): UTC offset of the time frame in format ±HHMM.
+            start_time (datetime | str): start time of the time frame.
+            end_time (datetime | str): end time of the time frame.
+        """
+
+        # Splitting the offset into hour and min.
+        self.offset_hour = int(utc_offset[:3])
+        self.offset_min = int(utc_offset[3:])
+
+        # UTC Offset.
+        sign = "+" if self.offset_hour >= 0 else "-"
+        self.utc_offset = f"{sign}{abs(self.offset_hour):02}:{self.offset_min:02}"
+
+        # Create datetime objects for start and end time.
+        self.start_time = datetime.strptime(start_time, IN_DATETIME_FORMAT) if type(start_time) is str else start_time
+        self.end_time = datetime.strptime(end_time, IN_DATETIME_FORMAT) if type(end_time) is str else end_time
+
+        # Check if the end time is earlier than start time.
+        if self.end_time < self.start_time:
+            raise ValueError("Illegal TimeFrame attributes: end time cannot be earlier than start time.")
+
+        # Create the time delta object. timedelta accepts negative values in parameters.
+        delta = timedelta(hours=self.offset_hour, minutes=self.offset_min)
+
+        # Calculate the normalized times.
+        self.norm_start_time = self.start_time - delta
+        self.norm_end_time = self.end_time - delta
+
+    def get_times(self) -> Tuple[datetime, datetime]:
+        """ Get the start and end times of the TimeFrame.
+
+        Returns:
+              Tuple of containing the start and end times.
+        """
+
+        return self.start_time, self.end_time
+
+    def get_norm_times(self) -> Tuple[datetime, datetime]:
+        """ Get the UTC+0000 normalized start and end times of the TimeFrame.
+
+        Returns:
+              Tuple containing the normalized start and end times.
+        """
+
+        return self.norm_start_time, self.norm_end_time
+
+    def get_utc_offset(self) -> str:
+        """ Get the UTC offset of the TimeFrame.
+
+        Returns:
+              a string of the UTC offset in the format ±HHMM.
+        """
+
+        return self.utc_offset
+
+    def get_times_str(self, datetime_format: str = OUT_DATETIME_FORMAT) -> Tuple[str, str]:
+        """ Get the start and end times of the TimeFrame as str.
+
+        Args:
+            datetime_format (str): Optional argument to modify the datetime string format. Default: DD-MM-YY HH:MM.
+
+        Returns:
+              Tuple of containing the start and end times as strings.
+        """
+
+        return self.start_time.strftime(datetime_format), self.end_time.strftime(datetime_format)
+
+    def get_norm_times_str(self, datetime_format: str = OUT_DATETIME_FORMAT) -> Tuple[str, str]:
+        """ Get the UTC+0000 normalized start and end times of the TimeFrame as str.
+
+        Args:
+            datetime_format (str): Optional argument to modify the datetime string format. Default: DD-MM-YY HH:MM.
+
+        Returns:
+              Tuple containing the normalized start and end times as strings.
+        """
+
+        return self.norm_start_time.strftime(datetime_format), self.norm_end_time.strftime(datetime_format)
