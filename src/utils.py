c8b2418b6a4b7fd14337c6f73e36da9809ad7096
CookieCrumbs19212 <developer19212@gmail.com>
2022-08-15 20:06:59 +0200

    Moved TimeFrame class from utils.py to timeframe.py
    Removed Timezone class.
    
    Removed IN_DATETIME_FORMAT.
    
    Created construct_timeframe_table() function.

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,12 @@
 import os
 import re
-from datetime import datetime, timedelta
 from typing import Tuple
 
 
 VALID_UTC_OFFSETS = ["-1200", "-1100", "-1000", "-0930", "-0900", "-0800", "-0700", "-0600", "-0500", "-0400",
-                     "-0330", "-0300", "-0200", "-0100", "+0000", "-0000", "+0100", "+0200", "+0300", "+0330",
+                     "-0330", "-0300", "-0200", "-0100", "-0000", "+0000", "+0100", "+0200", "+0300", "+0330",
                      "+0400", "+0430", "+0500", "+0530", "+0545", "+0600", "+0630", "+0700", "+0800", "+0845",
                      "+0900", "+0930", "+1000", "+1030", "+1100", "+1200", "+1245", "+1300", "+1400"]
-
-# Input Datetime formats.
-IN_DATETIME_FORMAT = '%d-%m-%y %H%M'
 
 
 def clear_screen() -> None:
@@ -18,8 +14,8 @@ def clear_screen() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def validate_datetime(in_datetime: str) -> bool:
-    """ Validate a datetime string's format.
+def is_valid_datetime(in_datetime: str) -> bool:
+    """ Checks if a datetime string has the format DD-MM-YYYY HHMM.
 
     Args:
         in_datetime (str): the datetime string input.
@@ -35,17 +31,17 @@ def validate_datetime(in_datetime: str) -> bool:
     return bool(datetime_re.match(in_datetime))
 
 
-def validate_offset(in_offset: str) -> Tuple[bool, str]:
+def is_valid_offset(input_offset: str) -> Tuple[bool, str]:
     """ Validate a UTC offset string.
 
     Args:
-        in_offset (str): the UTC offset string input.
+        input_offset (str): the UTC offset string input.
 
     Returns:
         a tuple (flag, error_message). flag is True if the input matches the ±HHMM format.
     """
 
-    # Flag is True if the utc_offset is valid.
+    # Flag is True if the input_offset is valid.
     flag = True
     # Error message to print to the terminal.
     error_message = ""
@@ -53,98 +49,79 @@ def validate_offset(in_offset: str) -> Tuple[bool, str]:
     # Regex expression for UTC offset.
     offset_re = re.compile(r"^[+,-]\d{4}$")
 
-    # Match in_datetime argument with datetime regex.
-    if not offset_re.match(in_offset):
+    # Match input_offset argument with offset regex.
+    if not offset_re.match(input_offset):
         flag = False
-        error_message = "add: Incorrect format of UTC offset. Expected format: ±HHMM."
+        error_message = "Incorrect format of UTC offset. Expected format: ±HHMM."
 
-    elif in_offset in VALID_UTC_OFFSETS:
+    elif input_offset in VALID_UTC_OFFSETS:
         flag = False
-        error_message = "add: Invalid UTC offset. Provide a valid UTC offset."
+        error_message = "Invalid UTC offset. Provide a valid UTC offset."
 
     # Return the flag and message.
     return flag, error_message
 
 
-class Timezone:
-    """
-    Timezone class.
-    """
+def construct_timeframe_table(timeframes: dict) -> str:
+    # Headers strings for each column.
+    column_headers = ["Timeframe ID", "UTC Offset", "Start Time", "End time",
+                      "Normalized Start Time", "Normalized End Time"]
 
-    def __init__(self, utc_offset: str) -> None:
-        """ Initialize a Timezone object.
+    # Determining the width of the Timeframe ID column.
+    # The width is the length of the longest timeframe_id or the length of the column header, whichever is longer.
+    timeframe_id_col_width = max(len(max(timeframes.keys(), key=len)), len(column_headers[0]))
 
-        Args:
-            utc_offset (str): Should be in format `±HHMM` in 24hr format. e.g. -0200, for UTC-2.
-        """
+    column_widths = [timeframe_id_col_width, 10, 14, 14, 21, 19]
 
-        assert type(utc_offset) is str and len(utc_offset) == 5
+    # delete timeframe_id_col_width.
+    del timeframe_id_col_width
 
-        self.hour = int(utc_offset[:3])
-        self.min = int(utc_offset[3:])
+    # # Appending the column widths for the remaining columns 1 to (N-1), where N is the number of columns.
+    # # Add 2 to the column width to account for 1 leading and 1 trailing space.
+    # column_widths = column_widths + [len(header) for header in column_headers[1:]]
 
-    def __str__(self):
-        """ String representation of Timezone object.
+    # Horizontal length of the output string.
+    # horizontal length = sum column widths  +
+    #                     1 leading and 1 trailing spaces in each column (= number_of_columns x 2) +
+    #                     number of column dividers "|" (= number_of_columns + 1)
+    horizontal_len = sum(column_widths) + len(column_widths)*2 + (len(column_widths) + 1)
 
-        Returns:
-              a string of the UTC offset in the format ±HHMM.
-        """
+    # Adding the starting horizontal line.
+    output_string = ("-" * horizontal_len) + "\n"
 
-        sign = "+" if self.hour >= 0 else "-"
-        return f"{sign}{self.hour}{self.min}"
+    # Adding the column headers' row.
+    for index, header in enumerate(column_headers):
+        output_string += f"| {header:{column_widths[index]}} "
+    output_string += "|\n"
 
+    # Adding the horizontal divider after the column headers.
+    # output_string += f"|{('-' * (horizontal_len - 2))}|\n"
 
-class TimeFrame:
-    """
-    TimeFrame class that encapsulates the UTC offset, start time and end time of a timeframe.
-    """
+    for width in column_widths:
+        output_string += f"|{('-' * (width+2))}"
+    output_string += "|\n"
 
-    def __init__(self, utc_offset: str, start_time: str, end_time: str) -> None:
-        """ Initialize a TimeFrame object with 3 mandatory parameters.
+    # Adding the timeframe ids and the respective timeframe information.
+    for timeframe_id, timeframe in timeframes.items():
+        # Getting the initial and normalized start and end times.
+        utc_offset = timeframe.get_utc_offset()
+        times = timeframe.get_times_str()
+        norm_times = timeframe.get_norm_times_str()
 
-        Args:
-            utc_offset (int): UTC offset of the time frame in format ±HHMM.
-            start_time (datetime): start time of the time frame.
-            end_time (datetime): end time of the time frame.
-        """
+        # Appending the timeframe_id.
+        output_string += f"| {timeframe_id:{column_widths[0]}} "
 
-        # Create a Timezone object.
-        self.timezone = Timezone(utc_offset)
+        # Appending the UTC offset.
+        output_string += f"| {utc_offset:{column_widths[1]}} "
 
-        # Create datetime objects for start and end time.
-        self.start_time = datetime.strptime(start_time, IN_DATETIME_FORMAT)
-        self.end_time = datetime.strptime(end_time, IN_DATETIME_FORMAT)
+        # Appending the start and end times.
+        output_string += f"| {times[0]:{column_widths[2]}} | {times[1]:{column_widths[3]}} "
 
-        # Create the time delta object. timedelta accepts negative values in parameters.
-        delta = timedelta(hours=self.timezone.hour, minutes=self.timezone.min)
+        # Appending the normalized start and end times. Adding the newline character at the end.
+        output_string += f"| {norm_times[0]:{column_widths[4]}} | {norm_times[1]:{column_widths[5]}} |\n"
 
-        # Calculate the normalized times.
-        self.norm_start_time = self.start_time - delta
-        self.norm_end_time = self.end_time - delta
+    # Add the ending line.
+    output_string += ("-" * horizontal_len) + "\n"
 
-    def get_times(self) -> Tuple[datetime, datetime]:
-        """ Get the start and end times of the TimeFrame.
-
-        Returns:
-              Tuple of containing the start and end times.
-        """
-
-        return self.start_time, self.end_time
-
-    def get_norm_times(self) -> Tuple[datetime, datetime]:
-        """ Get the UTC+00 normalized start and end times of the TimeFrame.
-
-        Returns:
-              Tuple of containing the normalized start and end times.
-        """
-
-        return self.norm_start_time, self.norm_end_time
-
-    def get_utc_offset(self) -> str:
-        """ Get the UTC offset of the TimeFrame.
-
-        Returns:
-              a string of the UTC offset in the format ±HHMM.
-        """
-
-        return str(self.timezone)
+    # Return the constructed string.
+    return output_string
