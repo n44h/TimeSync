d1e6e15a6d3b55b986ccc05bab2905ed136cd0aa
CookieCrumbs19212 <developer19212@gmail.com>
2022-08-21 14:08:50 +0200

    Updated VALID_UTC_OFFSETS strings to include the colon.
    Added format_time() function.
    
    Added format_utc_offset() function.
    
    Updated documentation: "HHMM" changed to "HH:MM".
    
    Refactored code to adopt updated time format: "HH:MM".

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,10 @@ import re
 from typing import Tuple
 
 
-VALID_UTC_OFFSETS = ["-1200", "-1100", "-1000", "-0930", "-0900", "-0800", "-0700", "-0600", "-0500", "-0400",
-                     "-0330", "-0300", "-0200", "-0100", "-0000", "+0000", "+0100", "+0200", "+0300", "+0330",
-                     "+0400", "+0430", "+0500", "+0530", "+0545", "+0600", "+0630", "+0700", "+0800", "+0845",
-                     "+0900", "+0930", "+1000", "+1030", "+1100", "+1200", "+1245", "+1300", "+1400"]
+VALID_UTC_OFFSETS = ["-12:00", "-11:00", "-10:00", "-09:30", "-09:00", "-08:00", "-07:00", "-06:00", "-05:00", "-04:00",
+                     "-03:30", "-03:00", "-02:00", "-01:00", "-00:00", "+00:00", "+01:00", "+02:00", "+03:00", "+03:30",
+                     "+04:00", "+04:30", "+05:00", "+05:30", "+05:45", "+06:00", "+06:30", "+07:00", "+08:00", "+08:45",
+                     "+09:00", "+09:30", "+10:00", "+10:30", "+11:00", "+12:00", "+12:45", "+13:00", "+14:00"]
 
 
 def clear_screen() -> None:
@@ -14,18 +14,188 @@ def clear_screen() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def format_time(time_str: str) -> str:
+    """ Rectifies incorrectly formatted start/end time strings.
+
+    Args:
+        time_str: the time string to format.
+
+    Returns:
+        the time string with the correct format.
+
+    """
+
+    """ 
+    NOTE: Correct time string format: HH:MM 
+    """
+
+    # Basic validations.
+    # time_str should not contain more than 1 colon character.
+    if time_str.count(":") > 1:
+        raise ValueError("time string cannot contain more than one \":\" character.")
+
+    # Besides one colon ":" character, time_str should only contain digits.
+    elif time_str.replace(":", "").isdigit() is False:
+        raise ValueError("time strings can only contain digits and \":\" characters.")
+
+    # Get the time string length.
+    length = len(time_str)
+
+    # Check 1
+    #
+    # Time string is empty or only contains a colon.
+    #
+    if time_str in ["", ":"]:
+        return "00:00"
+
+    # Check 2
+    #
+    # String contains only a single digit.
+    #
+    # Possible inputs:  4 -> 04:00
+    #
+    elif length == 1:
+        return f"0{time_str}:00"
+
+    # Check 3
+    #
+    # Contains 2 digits, assume the 2 digits refer to hour.
+    #
+    # Possible inputs:  12 -> 12:00
+    #                   04 -> 04:00
+    #
+    elif length == 2:
+        return f"{time_str}:00"
+
+    # Check 4
+    #
+    # Contains 3 digits.
+    #
+    # Assume first digit belongs to hour and last 2 digits belong to minutes.
+    #
+    # Possible inputs:  425 -> 04:25
+    #
+    elif length == 3 and ":" not in time_str:
+        return f"0{time_str[0]}:{time_str[1:]}"
+
+    # Check 5
+    #
+    # Contains 2 digits and a colon.
+    #
+    # Possible inputs:  :30 -> 00:30
+    #                   4:5 -> 04:05
+    #                   12: -> 12:00
+    #
+    elif length == 3 and ":" in time_str:
+        # Get the index of the colon character.
+        colon_index = time_str.index(":")
+
+        # For type: ":30" -> "00:30"
+        if colon_index == 0:
+            return f"00{time_str}"
+
+        # For type: "4:5" -> "04:05"
+        elif colon_index == 1:
+            return f"0{time_str[0:2]}0{time_str[-1]}"
+
+        # For type: "12:" -> "12:00"
+        else:
+            return f"{time_str}00"
+
+    # Check 6
+    #
+    # Contains 4 digits.
+    #
+    # First 2 digits belong to hour and last 2 digits belong to minutes.
+    #
+    # Possible inputs:  1125 -> 11:25
+    #
+    elif length == 4 and ":" not in time_str:
+        return f"{time_str[0:2]}:{time_str[2:]}"
+
+    # Check 7
+    #
+    # Contains 3 digits and a colon.
+    #
+    # Possible inputs:  1:30 -> 01:30
+    #                   14:5 -> 14:05
+    #                   :125 -> raise ValueError
+    #                   125: -> raise ValueError
+    #
+    elif length == 4 and ":" in time_str:
+        # Get the index of the colon character.
+        colon_index = time_str.index(":")
+
+        # For type: "1:30" -> "01:30"
+        if colon_index == 1:
+            return f"0{time_str}"
+
+        # For type: "14:5" -> "14:05"
+        elif colon_index == 2:
+            return f"{time_str[0:3]}0{time_str[-1]}"
+
+        # For type where colon is in index 0 or 3.
+        else:
+            raise ValueError("invalid time string format.")
+
+    # Check 8
+    #
+    # Contains 5 digits.
+    #
+    # Possible inputs:  15:00 -> valid input
+    #                   other -> raise ValueError
+    #
+    elif length == 5:
+        if time_str.index(":") == 2:
+            return time_str
+        else:
+            raise ValueError("invalid time format.")
+
+    # length > 5, raise ValueError.
+    else:
+        raise ValueError("invalid time format.")
+
+
+def format_utc_offset(utc_offset_str: str) -> str:
+    """ Rectifies incorrectly formatted UTC offset strings.
+
+    Args:
+        utc_offset_str: utc offset string to format.
+
+    Returns:
+        the UTC offset string with the correct format.
+
+    """
+
+    """ 
+    NOTE: Correct UTC offset string format: ±HH:MM
+    """
+
+    # If the utc_offset_str contains a sign.
+    if utc_offset_str[0] in ["+", "-"]:
+        sign = utc_offset_str[0]
+        time_str = format_time(utc_offset_str[1:])
+
+    # If sign is missing, assign sign as "+".
+    else:
+        sign = "+"
+        time_str = format_time(utc_offset_str)
+
+    return f"{sign}{time_str}"
+
+
 def is_valid_datetime(in_datetime: str) -> bool:
-    """ Checks if a datetime string has the format DD-MM-YYYY HHMM.
+    """ Checks if a datetime string has the format DD-MM-YYYY HH:MM.
 
     Args:
         in_datetime (str): the datetime string input.
 
     Returns:
-        True if the input matches the DD-MM-YY HHMM format.
+        True if the input matches the DD-MM-YY HH:MM format.
     """
 
     # Regex for datetime format.
-    datetime_re = re.compile(r"^[0-3]\d-[0-1]\d-\d{2} [0-2]\d[0-5]\d$")
+    datetime_re = re.compile(r"^[0-3]\d-[0-1]\d-\d{2} [0-2]\d:[0-5]\d$")
 
     # Match in_datetime argument with datetime regex.
     return bool(datetime_re.match(in_datetime))
@@ -38,7 +208,7 @@ def is_valid_offset(input_offset: str) -> Tuple[bool, str]:
         input_offset (str): the UTC offset string input.
 
     Returns:
-        a tuple (flag, error_message). flag is True if the input matches the ±HHMM format.
+        a tuple (flag, error_message). flag is True if the input matches the ±HH:MM format.
     """
 
     # Flag is True if the input_offset is valid.
@@ -47,7 +217,7 @@ def is_valid_offset(input_offset: str) -> Tuple[bool, str]:
     error_message = ""
 
     # Regex expression for UTC offset.
-    offset_re = re.compile(r"^[+,-]\d{4}$")
+    offset_re = re.compile(r"^[+,-]\d{2}:\d{2}$")
 
     # Match input_offset argument with offset regex.
     if not offset_re.match(input_offset):
@@ -63,6 +233,18 @@ def is_valid_offset(input_offset: str) -> Tuple[bool, str]:
 
 
 def construct_timeframe_table(timeframes: dict) -> str:
+    """ Constructs a table containing the timeframe IDs, UTC offsets, start/end times and normalized start/end times
+    of the timeframes.
+
+    Used in the 'list' action in TimeSync.
+
+    Args:
+        timeframes (dict): timeframes to include in the table.
+
+    Returns:
+        a table containing details about each timeframe as a multiline string.
+
+    """
     # Headers strings for each column.
     column_headers = ["Timeframe ID", "UTC Offset", "Start Time", "End time",
                       "Normalized Start Time", "Normalized End Time"]
